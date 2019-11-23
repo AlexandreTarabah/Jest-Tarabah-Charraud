f2928cff74ac533ffae13b045d51aeda4a6756f2
AlexandreTarabah <alexandre.tarabah@outlook.fr>
2019-11-23 17:58:18 +0100

    MAJ TrophyLowest

--- jest-tarabah-charraud/src/jest_tarabah_charraud_2019_2020/TrophyLowest.java
+++ jest-tarabah-charraud/src/jest_tarabah_charraud_2019_2020/TrophyLowest.java
@@ -1,16 +1,38 @@
 package jest_tarabah_charraud_2019_2020;
 
+import java.util.Iterator;
 
-public class TrophyLowest extends Trophy {
+public class TrophyLowest extends Trophy implements Visitor {
     public TrophyLowest(Value value, Color color) {
 		super(value, color);
 		// TODO Auto-generated constructor stub
 	}
 
-	public void lowest() {
-    }
 
-    public void visitJest(Jest p1) {
+    public void visitJest(Jest jest, Color color) {
+    	
+    	{
+    		Card highest = null ;
+    		int lowestOrdinal = 5 ;
+
+    		Iterator<Card> itJC = jest.jestCards.iterator() ; /* Entrance dans le
+    													   jest du joueur
+    		 */
+    		while(itJC.hasNext())
+    		{
+    			Card card = (Card) itJC.next();
+    			if(card.getColor().equals(color))
+    			{
+    				if(card.getValue().ordinal() < lowestOrdinal)
+    				{
+    					highest = card ; 
+    				}
+    			}
+
+
+    		}
+    	}
     }
+    
 
 }
