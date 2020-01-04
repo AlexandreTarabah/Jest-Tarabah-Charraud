d01d30844888c0cc4a98a3c375eab9577d5b9177
zorro <zorro@LAPTOP-L9H9L2K4.home>
2020-01-04 15:05:07 +0100

    maj

--- jest-tarabah-charraud/src/lanceur/Lanceur.java
+++ jest-tarabah-charraud/src/lanceur/Lanceur.java
@@ -1,5 +1,31 @@
 package lanceur;
 
-public class Lanceur {
+import javax.swing.UIManager;
 
+import controleur.Controleur;
+import modele.game.Game;
+import vue.*;
+
+public class Lanceur {
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(
+            UIManager.getCrossPlatformLookAndFeelClassName());
+		} 
+		catch (Exception e) {
+
+		}
+		Controleur controleur = new Controleur();
+		Game partie = new Game();
+		Home accueil = new Home();
+		Regles regles = new Regles();
+		Parametres parametres = new Parametres();
+		Plateau plateau = new Plateau(partie, controleur);
+
+		controleur.setModele(partie);
+		controleur.setVue(accueil, parametres, regles, plateau);
+		accueil.setListener(controleur);
+		regles.setListener(controleur);
+		parametres.setListener(controleur);
+	}
 }
