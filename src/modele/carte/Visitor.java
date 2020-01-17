a8f0fa020938a225870ac472c46d8ff196263d5f
zorro <zorro@LAPTOP-L9H9L2K4>
2020-01-17 18:24:17 +0100

    visitor javadoc maj

--- src/modele/carte/Visitor.java
+++ src/modele/carte/Visitor.java
@@ -1,7 +1,11 @@
 package modele.carte;
 
 import modele.tas.Jest;
-
+/**
+ * Interface qui visite le Jest. En effet l'application utilise le patron de conception visitor 
+ * @author zorro
+ *
+ */
 public interface Visitor 
 {
     void visitJest(Jest jest);
